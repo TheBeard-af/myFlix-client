f75bf3a944d81473168db4a8e97d0ec2af76791c
TheBeard-af <afaqnadeem1993@gmail.com>
2025-10-30 12:37:13 +1100

    Add react bootstrap dependency

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,8 @@
 import { createRoot } from "react-dom/client";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.scss";
+
 import { MainView } from "./components/main-view/main-view";
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
